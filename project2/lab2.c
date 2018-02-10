47e5e1988ebd722065359ed8cfbcf607c627b079
ThierrySt-Arnaud <haratiri@gmail.com>
2018-02-09 17:47:44 -0700

    Parsing now works. Added a lot of printf's to display variables.

--- project2/lab2.c
+++ project2/lab2.c
@@ -8,37 +8,31 @@
 #define MAX_LINE     80
 #define MAX_COMMANDS 5
 
-//bool compareString(char *[], char *[]);
 void ChildProcess(char *[]);//child process that performs execvp()
 void ParentProcess(void);//parent process that reads inputs for execvp()
 int extractArgs(char[], char *[]);
 
 int main(void)
 {
-	char *args[MAX_LINE/2 + 1];
+	char* args[MAX_LINE/2 + 1];
 
 	int should_run = 1;
 
 	int i, upper;
 
 	pid_t pid;
-	
-	
 
-	char exit[5];
-	strcpy(exit, "exit");
+	char exit[5] = "exit";
 
 	while(should_run)
 	{
 		printf("osh>");
 
-		char input[MAX_LINE];
+		char input[MAX_LINE+1];
 
-		//scanf("%[^\n]%*c", input);//record user input
-		//read(STDIN_FILENO, input, MAX_LINE);
-		if(fgets(input, MAX_LINE, stdin)!= NULL){
+		if(scanf("%[^\n]%*c", input) > 0){//record user input
 		    printf("Checking std.read: %s\n", input);
-		    if (0==strcmp(exit, input))
+		    if (strcmp(exit, input) == 0)
 		      {
 			printf("User input=exit\n");
 			should_run=0;
@@ -53,11 +47,14 @@ int main(void)
 			pid = fork();
 			if (pid==0)
 			  ChildProcess(args);
-			else
+			else if (pid > 0)
 			  ParentProcess();
+			else
+			  printf("An error occurred!\n");
 		      }
 		}
-		fflush(stdout); 
+		fflush(stdout);
+		int j = 0;
 	}
 
 	return 0;
@@ -75,41 +72,48 @@ void ParentProcess(void)
 	printf("In ParentProcess\n");
 }
 
-int extractArgs(char input[], char *args[])
+int extractArgs(char input[], char* args[MAX_LINE/2 + 1])
 {
 	char * token;
-	token = strtok(input," ");
+
+	//token = strtok(input," ");
 	printf("Extracting arguments\n");
 	int i = 0;
 
-	/*	while ((token=strsep(&input, " "))!=NULL)
+	while ((token=strsep(&input, " "))!=NULL)
 	{
-		malloc(sizeof(MAX_LINE/2 + 1));
+		int j = 0;
 		printf("Argument %i\n",i);
-		args[i++]=token;
-		}*/
-
-	while (token != NULL)
-	{
-		malloc(sizeof(MAX_LINE/2 + 1));
-		printf("Argument %i\n",i);
-		strcpy(*(args+i*(MAX_LINE/2 + 1)),token);
-		i++;
-		token = strtok(NULL," ");
+		while (token[j++] != '\0'){}
+		printf("Token is %s, and has %i characters\n",token,j);
+		if (( args[i] = (char*) malloc(j+1) )!=NULL){
+			strcpy(args[i++],token);
+		}
+		else{
+			printf("Unable to allocate memory!\n");
+			return -1;
+		}
 	}
+	args[i] = NULL;
 
-	int j =0;
-	while (j < i)
-	{
-		printf("%s\n",*(args+j*(MAX_LINE/2 + 1)));
-		j++;
+	int k = 0;
+	printf("i is %i, k is %i\n",i,k);
+	while (k < i){
+		printf("Argument %i is %s\n",k,args[k]);
+		k++;
 	}
-	int background = ('&'== *(*(args+(i-1)*(MAX_LINE/2 + 1))));
-	if (background)
-	{
+	printf("Checking for & in argument %i\n",i-1);
+	fflush(stdout);
+	char amperstring[2] = "&";
+	int background = 1;
+	
+	if (args[i-1] != NULL)
+		background = (strcmp(args[i-1],amperstring));
+
+	if (background == 0)
 		printf("& detected\n");
-	}
-
+	else
+		printf("& NOT detected\n");
 	//*counter=i;
 	return background;
 }
