a3c880df973fc5acf21ab83b4a55c80d5650dfb6
ThierrySt-Arnaud <haratiri@gmail.com>
2018-02-11 21:04:59 -0700

    Child now get out of the loop after executing, preventing infinite loops on exit. Back to using fgets but now drops the ending newline when input is read. Added a wait() in the parent process when & is detected and removes the & from the list of arguments. Lots of new console output to debug variable values.

--- project2/lab2.c
+++ project2/lab2.c
@@ -19,42 +19,59 @@ int main(void)
 	int should_run = 1;
 
 	int i, upper;
+	i = 0;
 
+	while (i < (MAX_LINE/2+1))
+		args[i++] = NULL;
+	i = 0;
+	
 	pid_t pid;
 
 	char exit[5] = "exit";
+	printf("should_run is %i\n", should_run);
 
 	while(should_run)
 	{
-		printf("osh>");
+		printf("%i osh>",getpid());
 
 		char input[MAX_LINE+1];
 
-		if(scanf("%[^\n]%*c", input) > 0){//record user input
-		    printf("Checking std.read: %s\n", input);
-		    if (strcmp(exit, input) == 0)
-		      {
-			printf("User input=exit\n");
-			should_run=0;
-			printf("exiting\n");
-			break;
-		      }
-		    else
-		      {
-			printf("User did not input exit\n");
-			int status = extractArgs(input, args);
+		if(fgets(input, sizeof(input), stdin) != NULL){ //read user input+ check errorr
+			input[strcspn(input,"\n")] = '\0';	// remove trailing newline
 			
-			pid = fork();
-			if (pid==0)
-			  ChildProcess(args);
-			else if (pid > 0)
-			  ParentProcess();
-			else
-			  printf("An error occurred!\n");
-		      }
+			printf("Checking std.read: %s\n", input);
+			
+			if (strcmp(exit, input) == 0){
+				printf("User input=exit\n");
+				should_run = 0;
+				printf("exiting\n");
+			} else {
+				printf("User did not input exit\n");
+				int status = extractArgs(input, args);
+				if (status < 0){
+					printf("An error occurred!\n");
+					return -1;
+				}
+				
+				pid = fork();
+				if (pid == 0){
+					ChildProcess(args);
+					break;
+				}	else if (pid > 0){
+					if (status != 1)
+						wait();
+					ParentProcess();
+				}	else	{
+					printf("Unable to create child proces!\n");
+					return -1;
+				}
+			}
+		} else{
+			printf("Input error!");
+			return -1;
 		}
+
 		fflush(stdout);
-		int j = 0;
 	}
 
 	return 0;
@@ -62,9 +79,8 @@ int main(void)
 
 void ChildProcess(char *args[])
 {
-	printf("In ChildProcess: input received is %s\n", *args);
+	printf("\nIn ChildProcess: command received is %s\n", *args);
 	execvp(args[0], args);
-
 }
 
 void ParentProcess(void)
@@ -72,20 +88,20 @@ void ParentProcess(void)
 	printf("In ParentProcess\n");
 }
 
-int extractArgs(char input[], char* args[MAX_LINE/2 + 1])
+int extractArgs(char input[MAX_LINE+1], char* args[MAX_LINE/2 + 1])
 {
 	char * token;
 
-	//token = strtok(input," ");
 	printf("Extracting arguments\n");
 	int i = 0;
 
-	while ((token=strsep(&input, " "))!=NULL)
+	while ((token=strsep(&input, " "))!=NULL && i < (MAX_LINE/2+1))
 	{
 		int j = 0;
 		printf("Argument %i\n",i);
 		while (token[j++] != '\0'){}
 		printf("Token is %s, and has %i characters\n",token,j);
+		free(args[i]);
 		if (( args[i] = (char*) malloc(j+1) )!=NULL){
 			strcpy(args[i++],token);
 		}
@@ -94,35 +110,28 @@ int extractArgs(char input[], char* args[MAX_LINE/2 + 1])
 			return -1;
 		}
 	}
+	free(args[i]);
 	args[i] = NULL;
 
-	int k = 0;
-	printf("i is %i, k is %i\n",i,k);
-	while (k < i){
-		printf("Argument %i is %s\n",k,args[k]);
-		k++;
+	int j = 0;
+	printf("i is %i, j is %i\n",i,j);
+	while (j < i){
+		printf("Argument %i is %s\n",j,args[j]);
+		j++;
 	}
 	printf("Checking for & in argument %i\n",i-1);
-	fflush(stdout);
-	char amperstring[2] = "&";
-	int background = 1;
 	
-	if (args[i-1] != NULL)
-		background = (strcmp(args[i-1],amperstring));
+	char amperstring[2] = "&";
+	int background = (strcmp(args[i-1],amperstring));
 
-	if (background == 0)
-		printf("& detected\n");
-	else
-		printf("& NOT detected\n");
-	//*counter=i;
-	return background;
+	if (background == 0){
+		printf("& detected");
+		free(args[i-1]);
+		args[i-1] = NULL;
+		return 1;
+	} else{
+		printf("& NOT detected");
+		return 0;
+	}
 }
-
-
-
-
-
-
-
-
 
